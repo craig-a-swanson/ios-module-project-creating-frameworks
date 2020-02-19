e7d8618e0859e96bdb6d50ca60750af9d3577c94
Craig Swanson <cswanson02@me.com>
2020-02-18 19:43:01 -0600

    Created framework file for LoadingUI and LoadingUITest project files.  Embedding the framework into the project and imported it in the view controller.

---
+++ LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Craig Swanson on 2/18/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
---
+++ LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Craig Swanson on 2/18/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
